bf539b41d33e689dbb3c0bf99f591c0c05ce0095
asadullah48 <asadullahshafique@hotmail.com>
2024-05-19 12:13:55 +0500

    edited

---
+++ Assigment1/ans19.ts
@@ -0,0 +1,4 @@
+//Dinner Guests: Use one of your programs from Exercises 14 through 18 to print a message 
+//indicating the number of people you are inviting to dinner.
+let friends: string[] = ["Usman", "Mansoor", "Mohsin", "Sultan", "Ifraheem", "Zakariyah", "Asadullah"];
+console.log(`I am inviting ${friends.length} friends for dinner & enjoyment.`);
